c91c7e7bbeba8d58da231db83ffb329f7e95d452
Kuro197948 <abc.197948.al@gmail.com>
2025-02-09 19:11:55 +0900

    Add files via upload

---
+++ htdocs/kadai_007/associative-array.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 連想配列に値を代入する
+       $Array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+       // 連想配列の値を出力する
+       print_r($Array);
+
+       // 改行する
+       echo '<br>';
+
+       /* 'age'というキーの値を更新する
+       $Array['name'] = 37;
+
+       // 新しく要素（キーと値）を追加する
+       $Array['price'] = '東京都';
+
+       // 連想配列の値を出力する
+       print_r($personal_data);*/
+       ?>
+   </p>
+</body>
